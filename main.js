834ff6d3faab9fd9811b30a2a7a3681334f50f0e
Hauke2222 <49106346+Hauke2222@users.noreply.github.com>
2022-12-04 22:01:08 +0100

    begin display board

--- main.js
+++ main.js
@@ -1,95 +1,96 @@
-const { createApp } = Vue;
+let table = 9;
+let boardTiles = [
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+];
 
-createApp({
-  data() {
-    return {
-      table: 9,
-      boardTiles: [
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-      ],
-    };
-  },
-  methods: {
-    shuffleBoard(array) {
-      let m = array.length;
-      let t, i;
+function shuffleBoard(array) {
+  let m = array.length;
+  let t, i;
+
+  // While there remain elements to shuffle…
+  while (m) {
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+
+    // And swap it with the current element.
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+}
+
+function genBoard(table) {
+  for (let i = 1; i < 11; i++) {
+    boardTiles[i - 1] = i;
+    boardTiles[i + 9] = "*";
+    boardTiles[i + 19] = table;
+    boardTiles[i + 29] = "=";
+    boardTiles[i + 39] = i * table;
+  }
+}
+
+function displayBoard(array) {
+  let wrapper = document.getElementsByClassName("wrapper")
+  console.log(wrapper);
+
+  for (let index = 0; index < array.length; index++) {
+    let div = document.createElement("div");
+    // div.innerHTML = array[index];
+    wrapper.appendChild(div)
     
-      // While there remain elements to shuffle…
-      while (m) {
-    
-        // Pick a remaining element…
-        i = Math.floor(Math.random() * m--);
-    
-        // And swap it with the current element.
-        t = array[m];
-        array[m] = array[i];
-        array[i] = t;
-      }
-    
-    },
-    genBoard(table){
-      for (let i = 1; i < 11; i++) {
-        this.boardTiles[i-1] = i;
-        this.boardTiles[i+9] = "*";
-        this.boardTiles[i+19] = table;
-        this.boardTiles[i+29] = "=";
-        this.boardTiles[i+39] = i * table;
-      }
-    }
-  },
-  computed: {},
-  mounted() {
-    this.genBoard(this.table);
-    this.shuffleBoard(this.boardTiles)
-    console.log(this.boardTiles);
-  },
-}).mount("#app");
+  }
+}
+
+genBoard(table);
+shuffleBoard(boardTiles);
+displayBoard(boardTiles);
+console.log(boardTiles);
