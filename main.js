9f34c28272a6908162f50267774bab5d7b5b69e7
Hauke2222 <49106346+Hauke2222@users.noreply.github.com>
2022-12-07 15:46:39 +0100

    add event listener + switch

--- main.js
+++ main.js
@@ -50,6 +50,7 @@ let boardTiles = [
   [],
   [],
   [],
+  "empty",
 ];
 
 function shuffleBoard(array) {
@@ -79,13 +80,41 @@ function genBoard(table) {
 }
 
 function displayBoard(array) {
-  let wrapper = document.getElementById("wrapper")
+  let wrapper = document.getElementById("wrapper");
   for (let index = 0; index < array.length; index++) {
     let div = document.createElement("div");
     div.innerHTML = array[index];
+    div.id = index;
     div.classList.add("box");
-    wrapper.appendChild(div)
-    
+    wrapper.appendChild(div);
+  }
+}
+
+function nextToEmptyTile(key) {
+  key = parseInt(key);
+  // console.log("test", key);
+  let empty = boardTiles.indexOf("empty");
+
+  console.log(typeof empty);
+  console.log(key, empty);
+
+  switch ("empty") {
+    case boardTiles[key + 1]:
+      console.log("+1");
+      break;
+    case boardTiles[key - 1]:
+      console.log("-1");
+      break;
+    case boardTiles[key + 10]:
+      console.log("+10");
+      break;
+    case boardTiles[key - 10]:
+      console.log("-10");
+      break;
+
+    default:
+      console.log("default");
+      break;
   }
 }
 
@@ -93,3 +122,12 @@ genBoard(table);
 // shuffleBoard(boardTiles);
 displayBoard(boardTiles);
 console.log(boardTiles);
+
+const onClick = (e) => {
+  console.log(e.target.id);
+  nextToEmptyTile(e.target.id);
+};
+
+document.querySelectorAll(".box").forEach((el) => {
+  el.addEventListener("click", onClick);
+});
